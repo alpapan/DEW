4c110f32a7f2761ce37adf5b0c55396b43690bc1
Alexie <alexie@butterflybase.org>
2013-01-28 12:41:50 +1100

    demos in separate repo

--- demoR/arabidopsis.demo.R
+++
@@ -1,5 +0,0 @@
-source('/home/pap056/workspace/dew/R/edgeR_funcs.R')
-targetsFile='arabidopsis.demo.files'
-datafile='tmp'; dispersion='auto'; FDR=0.05;kclusters = 10;minCPM = 2;minReps = 3
-edgeR_DE_analysis_explore(targetsFile,datafile,dispersion,FDR,kclusters,minCPM,minReps)
-
